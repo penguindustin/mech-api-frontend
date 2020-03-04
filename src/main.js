3eba62cf694f0aa7da819616edb625654aad41ce
Dustin Mendoza <penguindustin@gmail.com>
2020-03-04 09:38:44 -0800

    added vue-router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import VueRouter from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: HelloWorld}
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   vuetify,
   render: h => h(App)
 }).$mount('#app')

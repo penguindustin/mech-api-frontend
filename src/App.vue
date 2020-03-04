c958e84690effc2384fd619eb39e36dbab92cc81
Dustin Mendoza <penguindustin@gmail.com>
2020-03-04 09:26:18 -0800

    added app bar

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,40 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+    <v-app-bar app color="primary" dark absolute>
+      <div class="d-flex align-center"></div>
 
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      
+      <v-btn href="" target="_blank" text class="px-2">
+        <span class="mr-2 headline">Home</span>
       </v-btn>
+      <v-btn href="" target="_blank" text class="px-2">
+        <span class="mr-2 headline">About</span>
+      </v-btn>
+      <v-btn href="" target="_blank" text class="px-2">
+        <span class="mr-2 headline">Documentation</span>
+      </v-btn>
+      <v-spacer></v-spacer>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <HelloWorld />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    HelloWorld
   },
 
   data: () => ({
     //
-  }),
+  })
 };
 </script>
